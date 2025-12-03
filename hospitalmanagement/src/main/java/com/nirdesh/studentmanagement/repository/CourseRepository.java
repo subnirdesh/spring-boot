cd5cf4146136b762ae1ceab71b956817769e0664
nirdesh <nirdesh.academics@gmail.com>
2025-12-03 16:51:18 +0545

    chore: add exception handling

--- hospitalmanagement/src/main/java/com/nirdesh/studentmanagement/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nirdesh.studentmanagement.repository;
-
-import com.nirdesh.studentmanagement.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
